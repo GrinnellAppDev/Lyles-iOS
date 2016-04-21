e4b5492b128739a8873f90542a640c2dfb6ab816
Alex Mitchell <alex@alexthemitchell.com>
2016-04-21 18:42:38 -0500

    Add XCode project files

---
+++ Lyles/Lyles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lyles
+//
+//  Created by Alex Mitchell on 4/21/16.
+//  Copyright © 2016 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lyles/Lyles/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lyles
+//
+//  Created by Alex Mitchell on 4/21/16.
+//  Copyright © 2016 Grinnell AppDev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
