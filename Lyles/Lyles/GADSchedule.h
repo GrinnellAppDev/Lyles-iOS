a414afda16fa16a39b143263967da0046e509793
penglinq <linqingp@outlook.com>
2016-05-05 20:04:08 -0500

    Add tableview in the schedule part

---
+++ Lyles/Lyles/GADSchedule.h
@@ -0,0 +1,13 @@
+//
+//  GADSchedule.h
+//  Lyles
+//
+//  Created by AppDev on 5/5/16.
+//  Copyright © 2016 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GADSchedule : UIViewController
+
+@end
