704b79f7d17a037b55946f75395d8d73f80123de
Alex Mitchell <alex@alexthemitchell.com>
2016-11-21 20:22:19 -0600

    Reset to start with Beer Menu

---
+++ Lyles/Lyles/BeerMenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BeerMenuTableViewController.h
+//  Lyles
+//
+//  Created by Alex Mitchell on 11/21/16.
+//  Copyright © 2016 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BeerMenuTableViewController : UITableViewController
+
+@end
