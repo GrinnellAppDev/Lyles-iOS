d65f76d9876c50ab3bd8726f878548bc2cf661a3
Liam li <liam@Liams-MacBook-Pro.local>
2017-02-16 20:40:38 -0600

    Add Podfile

---
+++ Lyles/Cocopods/Cocopods.h
@@ -0,0 +1,19 @@
+//
+//  Cocopods.h
+//  Cocopods
+//
+//  Created by Liam li on 2/16/17.
+//  Copyright © 2017 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cocopods.
+FOUNDATION_EXPORT double CocopodsVersionNumber;
+
+//! Project version string for Cocopods.
+FOUNDATION_EXPORT const unsigned char CocopodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cocopods/PublicHeader.h>
+
+
