896908afa269606d13f0ad53c32ceda579c52972
wyk9787 <wangyika@grinnell.edu>
2017-02-16 20:16:37 -0600

    Add PubCore target

---
+++ PubCore/PubCore.h
@@ -0,0 +1,19 @@
+//
+//  PubCore.h
+//  PubCore
+//
+//  Created by Garrett Wang on 2/16/17.
+//  Copyright © 2017 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PubCore.
+FOUNDATION_EXPORT double PubCoreVersionNumber;
+
+//! Project version string for PubCore.
+FOUNDATION_EXPORT const unsigned char PubCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PubCore/PublicHeader.h>
+
+
